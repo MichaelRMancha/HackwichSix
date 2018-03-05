92047017ee1660287513f0cb3992549320ee368c
CM Student <cmstudent@uhwo-acm-15.local>
2018-03-05 10:35:00 -1000

    Completed parts 1 and 2 of the game assignment

--- HackwichSix/ViewController.swift
+++ HackwichSix/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var slider: UISlider!
+    var currentValue: Int = 0
+    
+    
+    @IBAction func sliderHasMoved(_ sender: Any) {
+        print("The value of the slider is:\(slider.value)")
+        currentValue = lroundf(slider.value)
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +32,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func myGuessButtonPressed(_ sender: Any) {
+        
+        let message = "The value is: \(currentValue)"
+        
+        let alert = UIAlertController(title:"Hello World", message: message, preferredStyle: .alert)
+        
+//creating the alert view
 
+//2.
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+    
+//3.
+        alert.addAction(action)
+        
+//4.
+        present(alert, animated: true, completion:nil)
+
+//5.
+        
+        
+    
+    }
+    
+    
 }
 
